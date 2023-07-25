31a111d3177fbfacb68f22993641751fd8acca24
Shivam Gupta <70855458+ShivamGupta92@users.noreply.github.com>
2023-07-25 12:03:13 +0530

    Update nfts.js

--- metadata/nfts.js
+++ metadata/nfts.js
@@ -1,28 +1,29 @@
 const nfts = [
   {
     name: "0",
-    description: "A cricketer wearing blue jersey cheering the crowd",
-    image: "QmdL362MF44qHLYVx4oUVvkSCpDNttSN3hTvPKUVm3aD8P",
+    description: "The bird of paradise or love birds represents faithfulness, love, and thoughtfulness.",
+    image: "QmQQqDLHixPhGFZMBPXU4c87W2NEX4crMPFzMGSmVJZTFh",
   },
   {
     name: "1",
-    description: "A cricketer wearing blue jersey cheering the crowd",
-    image: "QmYHUptBFLJbDjRBJxs3nsPtqYa3CSUNh3w8BeUTEMjFoQ",
+    description: "The bird of paradise or love birds represents faithfulness, love, and thoughtfulness.",
+    image: "QmYHKZTusHnygP93mZ6JQdHDESjrNDrFX9nPsbwrXHdiRx",
   }, //change
   {
     name: "2",
-    description: "A cricketer wearing blue jersey cheering the crowd",
-    image: "QmVYMcF5V8gKS8FeEC1xAe5rRpL6Q6kbjwGcGPpacUuNF5",
+    description: "The bird of paradise or love birds represents faithfulness, love, and thoughtfulness.",
+    image: "Qmd3rFnpSxqFXu2XiYj7XfBLuwYFHbPksavUTQaT8YG9xi",
+    
   },
   {
     name: "3",
-    description: "A cricketer wearing blue jersey cheering the crowd",
-    image: "QmbTd8m7HctVJr9A4FL83ejcMmyuAXbU4FkL3QERmZGiLM",
+    description: "The bird of paradise or love birds represents faithfulness, love, and thoughtfulness.",
+    image: "QmZiZ8TTqzr5kgPN7oxeeF4unmHAjGYZV3hcr3KuhcQJ4p",
   },
   {
     name: "4",
-    description: "A cricketer wearing blue jersey cheering the crowd.",
-    image: "QmRiB3kwnLP8BAudp6BNh2tfjKdwX4Hpmeigdrz45L9xAj",
+    description: "The bird of paradise or love birds represents faithfulness, love, and thoughtfulness.",
+    image: "QmfFob8sYx5TNLTdzsbwJabm3FGupTRypPqS96Ewwwf3Gb",
   },
 ];
 
